41c5548b380c0c12159d3e128f7bd1a26215ef22
Aai'sha Fisher <82815863+AaishaFisher@users.noreply.github.com>
2023-07-31 01:38:04 +0200

    number counter created

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-const number = document.querySelector('[data-key="number"]')
-const subtract = document.querySelector('[data-key="subtract"]')
-const add = document.querySelector('[data-key="add"]')
+const number = document.querySelector('[data-key="number"]');
+const subtract = document.querySelector('[data-key="subtract"]');
+const add = document.querySelector('[data-key="add"]');
 
+const subtractHandler = () => {
+  const newValue = parseInt(number.value) - 1;
+  number.value = newValue;
+};
+
+const addHandler = () => {
+    const newValue = parseInt(number.value) + 1;
+    number.value = newValue;
+};
+
+subtract.addEventListener("click", subtractHandler);
+add.addEventListener("click", addHandler);
